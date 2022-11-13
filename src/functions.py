d02edfbb86632a3f9a623b58c7c7de6a06e8fe05
Metudu <tarikucar@gmail.com>
2022-11-13 13:16:32 +0300

    Comment lines added to python files

--- src/functions.py
+++ src/functions.py
@@ -1,17 +1,17 @@
-import os
+import os                       # Import edilme amacı termalini silme işlemini gerçekleştirmek için
 
-row = [0,0,10,10]
-column = [5,4,5,4]
-empty_seats = [100,100,100,100]
-increse = [-1,-1]
-discount_rate_definer = []
-max_ticket_and_prices = []
+row = [0,0,10,10]               # Sırasıyla 1. 2. 3. ve 4. kategorilerin hangi satırdan başladığını gösterir
+column = [5,4,5,4]              # Sırasıyla 1. 2. 3. ve 4. kategorilerin hangi sütundan başladığını gösterir
+empty_seats = [100,100,100,100] # Sırasıyla 1. 2. 3. ve 4. kategorilerde boş olan koltuk sayısını gösterir
+increse = [-1,-1]               # 2. ve 4. kategorilerde koltuğun sağa veya sola gitmesini düzenler
+discount_rate_definer = []      # Kategorileri ve bilet sayısına bağlı olarak indirim oranlarını tutar
+max_ticket_and_prices = []      # İlk olarak maks bilet sayısını sonrasında da her kategorinin ücretinin ne kadar olduğunu tutar
 
-def clear():
+                                        
+def clear():                    # Terminali temizlemek için çağırılır
     os.system("cls||clear")    
-    # Terminali temizlemek için yazılan fonksiyon
 
-def main_menu(): # Main menu bu fonksiyonla çağırılacak ve kullanıcının seçtiği değer döndürülecektir
+def main_menu():                # Main menu bu fonksiyonla çağırılacak ve kullanıcının seçtiği değer döndürülecektir
     print("".center(41,"#"))
     print("1 - Make reservation")
     print("2 - Show Seats")
@@ -19,31 +19,52 @@ def main_menu(): # Main menu bu fonksiyonla çağırılacak ve kullanıcının s
     print("4 - Exit")
     selection = int(input("Enter the option: "))
 
-    while not  1 <= selection <= 4: # Kullanıcın seçtiği değerin doğru değerler arasında olup olmadığı kontrol ediliyor
+    while not  1 <= selection <= 4:     # Kullanıcın seçtiği değerin doğru değerler arasında olup olmadığı kontrol ediliyor
         print("Invalid option. Please try again.")
         selection = int(input("Enter the option: "))
     
-    if selection == 4:
+    if selection == 4:                  # Eğer kullanıcı çıkış yaptıysa 0 döndürerek main.py dosyasındaki while döngüsünü bititir
         print("You exited successfully!")
         return 0
 
-    return selection
+    return selection                    # Main menu fonksiyonu kullanıcının işlemini döndürür
 
-def make_reservation(cinema_seats):
-    discount_calculated = False
+def make_reservation(cinema_seats):     # Reservasyon yapmak için yazılmış fonksiyon
+    discount_calculated = False         # Indirim yapılıp yapılamadığını gösterir
     clear()
     print("You chose option 1")
     print("-"*20)
 
-    seat_category = int(input("Enter the seat category you want: "))    
-    while not  1<= seat_category <= 4:
+    seat_category = int(input("Enter the seat category you want: "))    # Kullanıcıdan istediği koltuk kategorisini alır
+    while not  1<= seat_category <= 4:                                  # Kullanıcı eğer istenmeyen bir değer girerse istenen değer girene kadar dönüyor
         print("Invalid option. Please try again.")
         seat_category = int(input("Enter the seat category you want: "))    
 
-    ticket_count = int(input("Enter the ticket count: "))
-    while not  1<= ticket_count <= int(max_ticket_and_prices[0][1]):
+    ticket_count = int(input("Enter the ticket count: "))               # Kullanıcıdan reservasyon yapılmak istenen koltuk sayısı alınır
+    while not  1<= ticket_count <= int(max_ticket_and_prices[0][1]):    # Kullanıcı eğer istenmeyen bit değer girerse istenen değer girene kadar devam eder    
         print("Invalid option. Please try again.")
         ticket_count = int(input("Enter the seat category you want: "))    
+
+    # Buradan sonraki while döngülerinin çalışma mantığı şöyledir:
+    # Tüm Kategoriler:
+        # Ilk olarak indirim değeri hesaplanıp ekrana bastırılır.
+        # Eğer boş koltuk kalmadıysa döngüden çıkılır.
+    # 1 ve 3. Kategoriler:
+        # Ikı durumda da sırasıyla başta tanımlanan satır ve sütun değerleri alınır ve oranın boş olup olmadıüı kontrol edilir. (- veya X)
+        # Eğer dolu ise(X) döngü dönmeye devam eder ve sütun değeri 1 artar.
+        # Eğer boş ise(-) değeri (X) e çevirilir, o kategorideki boş koltuk sayısı ve kullanıcının girdiği bilet sayısı 1 azaltılır.
+        # (Bilet sayısı da azaltıldığı için indirim oranında sıkıntı çıkmaması için öncelikle indirim değeri hesaplanır)
+        # Eğer o sıradaki koltuklar dolmuşsa bir alt satıra geçilir ve sütun değeri baştaki değere döner.
+        # Bilet sayısı 0a ulaştığında döngüden break ile çıkılır ve işelmin başarılı olduğu yazdırılır.
+
+    # 2. ve 4. Kategoriler:
+        # Ikı durumda da sırasıyla başta tanımlanan satır ve sütun değerleri alınır ve oranın boş olup olmadıüı kontrol edilir. (- veya X)
+        # Eğer dolu ise(X) döngü dönmeye devam eder ve sütun değeri koltukların sola veya sağa gitmesine göre (increase listesindeki değerler) 1 arttılır veya azaltılır.
+        # Eğer boş ise(-) değer (X) e çevirilir, o kategorideki boş koltuk sayısı 1 azaltılır ve (increase listesine göre) 1 sağa veya sola devam eder ve bilet sayısı 1 azaltılır.
+        # (Bilet sayısı da azaltıldığı için indirim oranında sıkıntı çıkmaması için öncelikle indirim değeri hesaplanır)
+        # Eğer sola doğru gidiliyor ve son koltuğa gelinmişse sağ tarafa geçilir (15. koltuk) ve döngü sağ tarafa doğru döner.(increase değeri - le çarpılır).
+        # Eğer sağa doğru gidiliyor ve son koltuğa gelinmiş ise bir alt satıra geçilir ve sütun değeri ve increase değeri ilk hale getirilir.
+        # Bilet sayısı 0a ulaştığında döngüden break ile çıkılır ve işelmin başarılı olduğu yazdırılır.
 
     if seat_category == 1:
         while True:
@@ -173,6 +194,23 @@ def make_reservation(cinema_seats):
 
 
 def define_discount_rate():
+
+    # Dosyada girilen değerleri başta boş olarak tanımlanan iki listeye atar.
+    # -----
+    # ...
+    # 3-60
+    # 4-40
+    # 1-5-10-05
+    # 1-10-15-10
+    # ...
+    # -----
+
+    # Satır okunduğunda ve sırasıyla elemanlar geçici bir listeye atıldığında liste boyutu 2 olursa (Örneğin 3-60 2 elemanlı (3,60))
+    # -> Değerleri liste olarak sırasıyla (max_ticket_and_prices) listeye atar
+
+    # Satır okunduğunda ve sırasıyla elemanlar geçici bir listeye atıldığında liste boyutu 4 olursa (Örneğin 1-05-10-05 4 elemanlı (1,05,10,05))
+    # -> Değerleri liste olarak sırasıyla (discount_rate_definer) listeye atar
+
     with open("discount.txt") as file:
         for line in file:
             if len(temp := line.rstrip().split("-")) == 2:
@@ -184,13 +222,18 @@ def define_discount_rate():
 
 
 def calculate_discount_rate(seat_category,ticket_count):
-    user_input = [str(seat_category),ticket_count]
+
+    # Parametre olarak atanılan koltuk kategorisi ve bilet sayısına göre indirimi belirler.
+    # Text dosyasına yazılan değerlerin okunduğu ve atandığı listeden(discount_rate_definer) listeleri çeker ve sırasıyla kategoriyi ve girilen 
+    # bilet sayısının aralığına bakar. Buna göre listeden indirim oranını çeker ve kullanıcıya yazdırır. Ayrıca seçilen koltuk kategorisinin ücretine
+    # indirim oranını uygular ve toplam tutarı ekrana yazdırarak kullanıcıyı bilgilendirir.
+
     for option in discount_rate_definer:
-        if option[0] == user_input[0] and int(option[1]) < ticket_count <= int(option[2]):
+        if option[0] == str(seat_category) and int(option[1]) < ticket_count <= int(option[2]):
             print(f"You have %{option[3]} discount! The cost is: {max_ticket_and_prices[seat_category][1]} - ({max_ticket_and_prices[seat_category][1]} * ({option[3]} / 100)) = {int(max_ticket_and_prices[seat_category][1])-(int(max_ticket_and_prices[seat_category][1]) * (int(option[3]) / 100))}")
              
 
-def print_seats(cinema_seats):
+def print_seats(cinema_seats):  #Dolu(X) veya boş(-) olduğu bilgisini kullanarak ekrana koltukları yazdırır. 
     clear()
     print("Seats:")
     for row in cinema_seats:
@@ -198,7 +241,7 @@ def print_seats(cinema_seats):
             print(seat,end=" ")
         print()
 
-def restart(cinema_seats):
+def restart(cinema_seats):      # Bütün sistemi ilk değerlerine getirir yani restart eder. Başta tanımlanmış bütün listeleri ilk haline geri getirir.
     clear()
     print("You restarted the program. Seats are all empty now!")
     cinema_seats[:] = [
--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
-from functions import clear,main_menu, make_reservation,print_seats,restart,define_discount_rate
+from functions import clear,main_menu, make_reservation,print_seats,restart,define_discount_rate # functions.py dosyasından fonksiyonları import eder
 
-clear()
-cinema_seats = [
+clear()             # Terminali temizleyip temiz bir görüntü oluşturur
+cinema_seats = [    # Sinema koltukları tanımlanır ve başlangıç olarak hepsi boştur(-)
     ["-"] * 20,
     ["-"] * 20,
     ["-"] * 20,
@@ -24,18 +24,18 @@ cinema_seats = [
     ["-"] * 20,
 ]
 
-print("CINEMA RESERVATION APPLICATION".center(41,"-"))
-define_discount_rate()
-selection = main_menu()
-while selection:
+print("CINEMA RESERVATION APPLICATION".center(41,"-"))  # Güzel bir görünüm vermesi için program adı başa yazılır ve center ile ortalanır
+define_discount_rate()                                  # Uygulama başında discount.txt dosyasından veriler okunur ve bu veriler için oluşturulan listelere atama yapılır
+selection = main_menu()                                 # Main menu komutu ile ana menü çağırılır ve kullanıcıdan alınan seçim dönüp selection değişkenine atanır
+while selection:                                        # Çıkış hariç tüm durumlar 0'dan farklı değerler döneceği için while selection çıkış hariç her an çalışır.
 
-    if selection == 1:
+    if selection == 1:                                  # Kullanıcı eğer rezervasyon yapmayı seçmiş ise make_reservation fonksiyonu çağırılır
         make_reservation(cinema_seats)
 
-    elif selection == 2:
+    elif selection == 2:                                # Kullanıcı eğer koltuk bilgisi almak istemiş ise print_seats fonksiyonu çağılır
         print_seats(cinema_seats)
 
     elif selection == 3:
-        restart(cinema_seats)
+        restart(cinema_seats)                           # Kullanıcı her şeyi sıfırlamak istemiş ise restart komutu çağırılır
 
-    selection = main_menu()
+    selection = main_menu()                             # Kullanıcı tekrar ana menüye atılarak hangi işlemi yapmak istediği belirlenir ve ona göre while döngüsü çalışmaya devam eder
